643b1f458c6bb3e31f795a9f65773727049f439f
can <cantopra@buffalo.edu>
2021-04-14 20:04:23 -0400

    virtual env

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
+from app import application
 
-@app.route('/')
-@app.route('/index')
+@application.route('/')
+@application.route('/index')
 def index():
 	return "Hello, World"
