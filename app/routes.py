096d7ca86a91a1384a818395f5db4fa92df1795c
can <cantopra@buffalo.edu>
2021-04-16 20:28:46 -0400

    test

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import application
+from flask import render_template
 
 @application.route('/')
 @application.route('/index')
 def index():
-	return "Hello, World"
+    return "Hello, World"
+    user = "Test User"
+    return render_template("base.html", name=user)
