c627cc83dcfb773013b7dce84f9558c7e5d7c8e2
JQuelen <40250516+JQuelen@users.noreply.github.com>
2021-05-02 00:06:47 -0400

    updated routes structure and added imports
    added LoginForm and RegistrationForm so application can properly render the forms from login and register .html files

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,25 @@
-from app import application
+from app import app
 from flask import render_template
+from forms import LoginForm
+from forms import RegistrationForm
 
 temp_db = {"user": "  "}
 
-@application.route('/')
-@application.route('/index')
-@application.route('/index.html')
+@app.route('/')
+@app.route('/index')
+@app.route('/index.html')
 def index():
     user = temp_db["user"]
     return render_template("test.html", temp_db=user)
 
 @application.route('/login', methods=["GET", "POST"])
 def login():
+    form = LoginForm()
     #user = temp_db["user"]
     #temp_db["user"] = methods["POST"].username
     return render_template("login.html")
 
 @application.route("/register", methods=["GET", "POST"])
 def register():
+    form = RegistrationForm
     return render_template("register.html")
