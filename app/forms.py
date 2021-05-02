ef71bcdf4ad20a7cd58525873d4cc9672aaed135
JQuelen <40250516+JQuelen@users.noreply.github.com>
2021-05-02 00:09:43 -0400

    added config and forms files
    forms.py constructs the forms that are being used (login and register). config allows the form to be configured

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Login')   
