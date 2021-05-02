c8f6172bfeb0dc260f2d26ffe66c0685d0a577f6
JQuelen <40250516+JQuelen@users.noreply.github.com>
2021-05-02 00:27:27 -0400

    fixed routes
    added forms to login and register. changed index file to index.html. temp name "ted" as placeholder

--- app/routes.py
+++ app/routes.py
@@ -10,16 +10,16 @@ temp_db = {"user": "  "}
 @app.route('/index.html')
 def index():
     user = temp_db["user"]
-    return render_template("test.html", temp_db=user)
+    return render_template("index.html", name="ted")
 
 @application.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
     #user = temp_db["user"]
     #temp_db["user"] = methods["POST"].username
-    return render_template("login.html")
+    return render_template("login.html",form=form)
 
 @application.route("/register", methods=["GET", "POST"])
 def register():
-    form = RegistrationForm
-    return render_template("register.html")
+    form = RegistrationForm()
+    return render_template("register.html",form=form)
