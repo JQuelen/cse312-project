b4cf5f520506962a9be9275242f3db2f9f253c68
JQuelen <40250516+JQuelen@users.noreply.github.com>
2021-05-02 00:24:55 -0400

    fixed registration form
    changed SubmitField from 'Login' to 'Sign Up'

--- app/forms.py
+++ app/forms.py
@@ -11,4 +11,4 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired()])
-    submit = SubmitField('Login')   
+    submit = SubmitField('Sign Up')   
